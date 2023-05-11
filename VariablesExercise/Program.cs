cb145707dd0c733c9dd9d6f61194db786469a8a4
Danamarie10 <dana.patrick10@outlook.com>
2023-05-11 12:56:26 -0400

    Variables exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Dana";
+            int myAge = 27;
+            char myFirstLetter = 'D';
+            bool coding = true;
+            double myMoney = 25.0;
+            decimal mySwag = 100.5M;
+
+            Console.WriteLine($"hi there my name is {myName}. I am {myAge} and the first letter of my name is {myFirstLetter}. I currently only have {myMoney} dollars but it is {coding} I am coding and my swag is {mySwag}");
         }
     }
 }
